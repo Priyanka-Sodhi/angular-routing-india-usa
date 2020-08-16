579a93d1cd5bfa1fe5f41a85de11b06040b04247
Priyanka-Sodhi <priyankasodhi00@gmail.com>
2020-08-16 23:08:32 +0530

    1 file modified, 4 added

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { IndiaUsaComponent } from '../india-usa/india-usa.component';
+import { AtlanticComponent } from '../atlantic/atlantic.component';
+import { PacificComponent } from '../pacific/pacific.component';
+
+const routes: Routes = [
+  {
+    path:"",    //base url
+    redirectTo:"india-usa",
+    pathMatch:"full"
+  },
+  {
+    path:"india-usa",
+    component:IndiaUsaComponent,
+    children:[
+      {
+        path:'atlantic',
+        component:AtlanticComponent
+      },
+      {
+        path:'pacific',
+        component:PacificComponent
+      }
+    ]
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
